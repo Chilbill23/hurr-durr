ef99a9681a73474d8d2b110e8e6d051600fb49d7
Chilbill23 <116380735+Chilbill23@users.noreply.github.com>
2022-11-11 15:54:36 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# hurr-durr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/hurr-durr-com)
---
+++ index.js
@@ -0,0 +1,6 @@
+button = document.querySelector("button")
+
+button.addEventListener("click", () => {
+	alert("hey")
+  })
+  console.log ("Hello,World!")
